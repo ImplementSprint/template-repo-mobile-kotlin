394b93a872ffda5a8bf9b1d4baa14d8b018d7340
lloydemersonlim888@gmail.com <lloydemersonlim888@gmail.com>
2026-04-04 13:58:55 +0800

    Fix eslint

--- app/src/test/java/com/implementsprint/mobile/SampleKotestSpec.kt
+++ app/src/test/java/com/implementsprint/mobile/SampleKotestSpec.kt
@@ -3,8 +3,9 @@ package com.implementsprint.mobile
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 
-class SampleKotestSpec : FunSpec({
-    test("template sanity check") {
-        (2 + 2) shouldBe 4
-    }
-})
+class SampleKotestSpec :
+    FunSpec({
+        test("template sanity check") {
+            (2 + 2) shouldBe 4
+        }
+    })
